c8bce4806f1083e752db0293d519003f99aff85e
koushiro <koushiro.cqx@gmail.com>
2019-01-29 12:21:14 +0800

    Add script
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

---
+++ sync-block.sh
@@ -0,0 +1,5 @@
+rm nohup.out
+rm -rf data
+ps -ef | grep 'chainx --dev -d data --port 20001 --bootnodes=/ip4/127.0.0.1/tcp' | grep -v grep | awk '{print $2}' | xargs kill -9
+echo FLUSHALL | redis-cli
+nohup ./chainx --dev -d data --port 20001 --bootnodes=/ip4/127.0.0.1/tcp/30000/p2p/QmPrQbZVFkSd1Ffof1LGoLHF2Qq8jqoUsQNEmcAzwaE426 &
