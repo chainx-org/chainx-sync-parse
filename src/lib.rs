3ae707b2e8cf70a3c68b75a73f5da40735a18f4f
koushiro <koushiro.cqx@gmail.com>
2019-02-11 16:35:53 +0800

    replace error-chain with failure
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-#[macro_use]
-extern crate error_chain;
-
 mod error;
 mod parse;
 mod register;
@@ -12,9 +9,10 @@ use std::sync::Arc;
 
 use parking_lot::RwLock;
 
-pub type BlockQueue = Arc<RwLock<BTreeMap<u64, Vec<serde_json::Value>>>>;
 pub use self::error::{Error, Result};
 pub use self::parse::RuntimeStorage;
 pub use self::register::RegisterService;
 pub use self::serde_ext::Bytes;
 pub use self::subscribe::RedisClient;
+
+pub type BlockQueue = Arc<RwLock<BTreeMap<u64, Vec<serde_json::Value>>>>;
