3280ab0d6fcbc5534ec7e9d1ef2f20f4656972e9
koushiro <koushiro.cqx@gmail.com>
2019-02-19 13:40:50 +0800

    update sync-block.sh
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- sync-block.sh
+++ sync-block.sh
@@ -1,5 +1,5 @@
-ps -ef | grep 'chainx --dev -d data --port 20001 --bootnodes=/ip4/127.0.0.1/tcp' | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep 'chainx -d data --chain=local --port 20001 --pruning archive --bootnodes=/ip4/127.0.0.1/tcp' | grep -v grep | awk '{print $2}' | xargs kill -9
 rm -rf data
 rm nohup.out
 echo FLUSHALL | redis-cli
-nohup ./chainx --dev -d data --port 20001 --bootnodes=/ip4/127.0.0.1/tcp/30000/p2p/QmPrQbZVFkSd1Ffof1LGoLHF2Qq8jqoUsQNEmcAzwaE426 &
+nohup ./chainx -d data --chain=local --port 20001 --pruning archive --bootnodes=/ip4/127.0.0.1/tcp/31120/p2p/QmXDuEABSsKmSg7SZd56ZrJrshmms6xPKvDrNe8stPgR9B &
