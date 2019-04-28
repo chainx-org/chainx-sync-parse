9dbe3f53744dd4a88612f491435250fe710622a7
koushiro <koushiro.cqx@gmail.com>
2019-04-28 15:00:26 +0800

    Add tail example
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- src/sync/tail.rs
+++ src/sync/tail.rs
@@ -9,7 +9,7 @@ use regex::bytes::Regex;
 use crate::Result;
 
 lazy_static::lazy_static! {
-    static ref RE: Regex = Regex::new(r"(?-u)msgbus\|height:\[(\d*)]\|key:\[(.*)]\|value:\[(.*)]").unwrap();
+    static ref RE: Regex = Regex::new(r"(?-u)INFO msgbus\|height:\[(\d*)]\|key:\[(.*)]\|value:\[(.*)]").unwrap();
 }
 
 const BUFFER_SIZE: usize = 1 << 8;
@@ -66,94 +66,60 @@ fn filter_line(line: &[u8]) -> Option<StorageData> {
         // key and value will be hex
         let key = hex::decode(&caps[2]).unwrap();
         let value = hex::decode(&caps[3]).unwrap();
-        //        let key = (&caps[2]).to_vec();
-        //        let value = (&caps[3]).to_vec();
         Some((height, key, value))
     } else {
         None
     }
 }
 
-#[test]
-fn test_tail() -> Result<()> {
-    let path = std::path::Path::new("./tail.log");
-    assert!(path.is_file());
-    let file = File::open(path)?;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let tail = Tail::new();
-    let handle = tail.run(file)?;
-
-    while let Ok((height, key, value)) = tail.recv_data() {
+    #[test]
+    fn test_regex_capture_value() {
+        // StorageValue - XBridgeOfBTC BestIndex
+        let map = "INFO msgbus|height:[0]|key:[584272696467654f664254432042657374496e646578]|value:[267334e6d27bbdf2cbcd07c97675363c8c63f44c3e7ef0384a00000000000000]\n";
+        let caps = RE.captures(map.as_bytes()).unwrap();
         println!(
-            "height = {:?}, key = {:?}, value = {:?}",
-            height,
-            String::from_utf8(key).unwrap(),
-            String::from_utf8(value).unwrap(),
+            "height: {:?}\nkey: {:?}\nvalue = {:?}\n",
+            &caps[1], &caps[2], &caps[3]
+        );
+        let height = std::str::from_utf8(&caps[1])
+            .unwrap()
+            .parse::<u64>()
+            .unwrap();
+        let key = String::from_utf8(hex::decode(&caps[2]).unwrap()).unwrap();
+        let value = hex::decode(&caps[3].to_vec()).unwrap();
+        assert_eq!(height, 0);
+        assert_eq!(key, "XBridgeOfBTC BestIndex".to_string());
+        assert_eq!(
+            value,
+            hex::decode("267334e6d27bbdf2cbcd07c97675363c8c63f44c3e7ef0384a00000000000000")
+                .unwrap(),
         );
     }
 
-    handle.join().expect("Join should be successful");
-    Ok(())
-}
-
-#[test]
-fn test_regex_capture() {
-    let re = Regex::new(r"(?-u)msgbus\|height:\[(\d*)]\|key:\[(.*)]\|value:\[(.*)]").unwrap();
-
-    // StorageMap
-    let map = b"msgbus|height:[123]|key:[XAssets AssetBalance\xa20\x81\x87C\x9a\xc2\x04\xdf\x9e)\x9e\x1eT\xaf\xef\xaf\xeaK\xf3H\xe0=\xadg\x977\xc9\x18q\xdcS\x0cPCX#123]|value:[\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x80|\x81J\x00\x00\x00\x00]\n";
-    let caps = re.captures(map).unwrap();
-    println!(
-        "height: {:?}\nkey: {:?}\nvalue = {:?}",
-        &caps[1], &caps[2], &caps[3]
-    );
-    let height = std::str::from_utf8(&caps[1])
-        .unwrap()
-        .parse::<u64>()
-        .unwrap();
-    let key = (&caps[2]).to_vec();
-    let value = (&caps[3]).to_vec();
-    assert_eq!(height, 123);
-    assert_eq!(
-        key,
-        vec![
-            88, 65, 115, 115, 101, 116, 115, 32, 65, 115, 115, 101, 116, 66, 97, 108, 97, 110, 99,
-            101, 162, 48, 129, 135, 67, 154, 194, 4, 223, 158, 41, 158, 30, 84, 175, 239, 175, 234,
-            75, 243, 72, 224, 61, 173, 103, 151, 55, 201, 24, 113, 220, 83, 12, 80, 67, 88, 35, 49,
-            50, 51
-        ]
-    );
-    assert_eq!(
-        value,
-        vec![2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 128, 124, 129, 74, 0, 0, 0, 0]
-    );
-
-    // StorageValue
-    let value = b"msgbus|height:[123]|key:[XBridgeOfBTC BestIndex#123]|value:[T\xdd\x8c\xc9\xea\xc2\x80\xcc\x98&\\\x07\x9dvAH7\x89+;\xcf\x88\x9b\\k\x0e\x00\x00\x00\x00\x00\x00]\n";
-    let caps = re.captures(value).unwrap();
-    println!(
-        "height: {:?}\nkey: {:?}\nvalue = {:?}",
-        &caps[1], &caps[2], &caps[3]
-    );
-    let height = std::str::from_utf8(&caps[1])
-        .unwrap()
-        .parse::<u64>()
-        .unwrap();
-    let key = (&caps[2]).to_vec();
-    let value = (&caps[3]).to_vec();
-    assert_eq!(height, 123);
-    assert_eq!(
-        key,
-        vec![
-            88, 66, 114, 105, 100, 103, 101, 79, 102, 66, 84, 67, 32, 66, 101, 115, 116, 73, 110,
-            100, 101, 120, 35, 49, 50, 51
-        ]
-    );
-    assert_eq!(
-        value,
-        vec![
-            84, 221, 140, 201, 234, 194, 128, 204, 152, 38, 92, 7, 157, 118, 65, 72, 55, 137, 43,
-            59, 207, 136, 155, 92, 107, 14, 0, 0, 0, 0, 0, 0
-        ]
-    );
+    #[test]
+    fn test_regex_capture_map() {
+        // StorageMap - XAssets AssetInfo\u{c}PCX
+        let value = "INFO msgbus|height:[0]|key:[58417373657473204173736574496e666f0c504358]|value:[0c5043583c506f6c6b61646f7420436861696e58000800b0436861696e5827732063727970746f2063757272656e637920696e20506f6c6b61646f742065636f6c6f6779010000000000000000]\n";
+        let caps = RE.captures(value.as_bytes()).unwrap();
+        println!(
+            "height: {:?}\nkey: {:?}\nvalue = {:?}",
+            &caps[1], &caps[2], &caps[3]
+        );
+        let height = std::str::from_utf8(&caps[1])
+            .unwrap()
+            .parse::<u64>()
+            .unwrap();
+        let key = String::from_utf8(hex::decode(&caps[2]).unwrap()).unwrap();
+        let value = hex::decode(&caps[3].to_vec()).unwrap();
+        assert_eq!(height, 0);
+        assert_eq!(key, "XAssets AssetInfo\u{c}PCX".to_string());
+        assert_eq!(
+            value,
+            hex::decode("0c5043583c506f6c6b61646f7420436861696e58000800b0436861696e5827732063727970746f2063757272656e637920696e20506f6c6b61646f742065636f6c6f6779010000000000000000").unwrap(),
+        );
+    }
 }
