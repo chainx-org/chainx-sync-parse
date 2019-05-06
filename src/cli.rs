b9e38bfe261cffec729ab2851ad43eff04561814
koushiro <koushiro.cqx@gmail.com>
2019-05-06 23:22:28 +0800

    Add data dir for testing
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- src/cli.rs
+++ src/cli.rs
@@ -38,7 +38,7 @@ pub struct Cli {
 
     /// Specify the sync log path
     #[cfg(feature = "sync-log")]
-    #[structopt(long = "sync-log", value_name = "PATH", default_value = "nohup.out")]
+    #[structopt(long = "sync-log", value_name = "PATH", default_value = "data/data.log")]
     pub sync_log_path: String,
 
     /// Specify the block height to start syncing
