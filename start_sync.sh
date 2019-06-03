76ab5381d45ced485c2a1e402b338928c2ed9720
koushiro <koushiro.cqx@gmail.com>
2019-06-03 23:59:51 +0800

    Add logrotate config
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- start_sync.sh
+++ start_sync.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-CUR_DATETIME="`date +%Y-%m-%d-%H`"
-LOG_NAME="sync.log.$CUR_DATETIME"
-echo "Current log name: $LOG_NAME"
-./chainx -d data --log=error,msgbus=info --name=cqx --port 20001 --pruning archive \
+work_path="/home/koushiro/Code/Work/bin"
+name="cqx"
+${work_path}/chainx --name=${name} -d ${work_path}/data --log=error,msgbus=info --port 20001 --pruning archive \
     --rpc-port=8098 --ws-port=8099 --rpc-external --ws-external --no-grandpa \
-    --syncing-execution=Native --no-telemetry &>> log/$LOG_NAME
+    --syncing-execution=Native --no-telemetry &>> ${work_path}/log/sync.log
